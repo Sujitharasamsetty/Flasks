d41645637fa2871c964ac8440ff0c2ef4aa8a7f4
sujitharasamsetty.sujitha=Rasamsetty <sujitharasamsetty@gmail.com>
2022-04-07 00:30:04 +0530

    Dynamic Routing

---
+++ app_4/app_4.py
@@ -0,0 +1,51 @@
+from atexit import register
+from flask import Flask
+
+app = Flask(__name__)
+
+registered_users=['Suji','Veda','Seku','Mom']
+
+@app.route('/')
+
+def home():
+    return "Welcome Home Page"
+
+
+'''@app.route('/in/Suji')
+
+def fun_user_1():
+    return "Welcome {} !".format(registered_users[0])
+
+@app.route('/in/veda')
+
+def fun_user_2():
+    return "Welcome {} !".format(registered_users[1])
+
+@app.route('/in/Seku')
+
+def fun_user_3():
+    return "Welcome {} !".format(registered_users[2])
+
+@app.route('/in/Mom')
+
+def fun_user_4():
+    return "Welcome {} !".format(registered_users[3])
+
+app.run()'''
+
+
+@app.route('/in/<user_name>')
+
+def user_details(user_name):
+    
+    if user_name in registered_users :
+        
+        return "Welcome {} !".format(user_name)
+    
+    else:
+        
+        return "Normusukoni.Velli Register Chesuko"
+    
+app.run()
+
+
