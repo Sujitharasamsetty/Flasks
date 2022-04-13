7f0e003ce6ba143df7f99cdc4cfcd88bd020d8f5
sujitharasamsetty.sujitha=Rasamsetty <sujitharasamsetty@gmail.com>
2022-04-13 21:15:12 +0530

    Jinja Template

---
+++ Jinja Template/app8.py
@@ -0,0 +1,24 @@
+from distutils.log import debug
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def fun_1():
+    return render_template('index.html')
+
+@app.route('/about')
+
+def fun_2():
+    
+    # we dont know how to fetch data from database
+    # So,we will store something inside a variable
+    
+   # vegatables_list = ['Aloo','Mushrooms','Bindi','lemon']
+    
+    vegetables_dict = {'Aloo' :50,'Mushrooms':100,'Bindi':60,'lemon':200}
+    return render_template('about_us.html',input = vegetables_dict)
+
+
+app.run(debug=True)
